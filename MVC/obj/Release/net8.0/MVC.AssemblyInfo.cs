3596426d4a52b4b806dea28f16bcfbadffd3cc19
cocosofia <est.sofia.marin@unimilitar.edu.co>
2025-10-14 18:12:53 -0500

    responsive muestras

--- MVC/obj/Release/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Release/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070a068e13208ebab6d2986e51aa2ba4e013a6c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
